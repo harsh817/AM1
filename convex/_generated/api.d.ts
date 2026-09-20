1af7c3e76d73ba90e15f1475f692fcc2c3f6d960
Harsh <harsh817@users.noreply.github.com>
2026-09-20 20:38:53 +0530

    Allow Convex auth websocket connections

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -11,6 +11,7 @@
 import type * as auth from "../auth.js";
 import type * as dashboard from "../dashboard.js";
 import type * as events from "../events.js";
+import type * as http from "../http.js";
 import type * as setup from "../setup.js";
 
 import type {
@@ -23,6 +24,7 @@ declare const fullApi: ApiFromModules<{
   auth: typeof auth;
   dashboard: typeof dashboard;
   events: typeof events;
+  http: typeof http;
   setup: typeof setup;
 }>;
 
